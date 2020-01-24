067b743ef28d5bc448e51d55210b01374928553d
javier051994 <60241573+javier051994@users.noreply.github.com>
2020-01-23 22:13:41 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>Porn4Fuck</title>
+     <script type="text/javascript" src="http://pop.porn4fuck.com/header.js"></script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-40159876-10"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-40159876-10');
+</script>
+    
+     <script language="JavaScript" type="text/javascript">
+
+var pagina="https://google.com"
+function redireccionar() 
+{
+location.href=pagina
+} 
+setTimeout ("redireccionar()", 0);
+
+</script>
+  </head>
+<body>
+
+  
+   <script language="JavaScript" type="text/javascript">
+
+var pagina="https://google.com"
+function redireccionar() 
+{
+location.href=pagina
+} 
+setTimeout ("redireccionar()", 0);
+
+</script>
+  
+</body>
+</html>
